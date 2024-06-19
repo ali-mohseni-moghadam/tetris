fc0171238c7b3baab2067e856748d15a49a24ee9
ali mohseni moghadam <mohsenimoghadamali@gmail.com>
2024-06-19 23:03:27 +0330

    change UI of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="containerDiv">
-      <h1 className="mb-10">| Welcom to Tetris Game |</h1>
+    <main className="containerDiv flex-col">
+      <h1 className="text-bold mb-16 text-4xl">| Welcom to Tetris Game |</h1>
 
       <div className="flex gap-x-5">
-        <button className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20">
+        <button className="rounded-full border border-gray-600 bg-gray-700 px-10 py-3 transition duration-300 hover:bg-gray-900 hover:text-orange-500">
           <Link href="/signup">Sign Up</Link>
         </button>
 
-        <button className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20">
+        <button className="rounded-full border border-gray-600 bg-gray-700 px-10 py-3 transition duration-300 hover:bg-gray-900 hover:text-orange-500">
           <Link href="/signin">Sign In</Link>
         </button>
 
-        <button className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20">
+        <button className="rounded-full border border-gray-600 bg-gray-700 px-10 py-3 transition duration-300 hover:bg-gray-900 hover:text-orange-500">
           <Link href="/game">Start Game</Link>
         </button>
       </div>
